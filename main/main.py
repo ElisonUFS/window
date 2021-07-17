e1bc72177d3590fe9289cc50a18ccda768d58f6f
Wilson Filho <will.flho@gmail.com>
2021-07-16 23:37:21 -0300

    feat: rearrange json structure and refactor code to uses new json structure

--- main/main.py
+++ main/main.py
@@ -1,42 +1,45 @@
-
 from tkinter import *
 
 from tkinter import messagebox as mb
+from PIL import ImageTk, Image
 
 import json
 
 #classe que define os componentes da GUI
-class Quiz:
+class Quiz(Frame):
 # Este é o primeiro método que é chamado quando um
 # novo objeto da classe é inicializado. Este método
 # define a contagem de perguntas para 0. e inicializa todos os
 # outras funções para exibir o conteúdo e fazer com que todas as
 # funcionalidade disponível
-	def __init__(self):
+	def __init__(self, questions):
+		super().__init__()
+
+		self.rd_buttons = []
+		self.questions = questions
 		
 		# definir o número da pergunta para 0
 		self.q_no=0
 		
-		# atribui perguntas à função display_question para atualizar mais tarde.
-		self.display_title()
-		self.display_question()		
-
 		# opt_selected contém um valor inteiro que é usado para
 		# opção selecionada em uma questão.
 		self.opt_selected=IntVar()
+		# atribui perguntas à função display_question para atualizar mais tarde.
+		self.display_title()
+		self.display_question()		
 		
 		# exibindo botão de opção para a pergunta atual e usado para
 		# opções de exibição para a pergunta atual
-		self.opts=self.radio_buttons()
+		# self.opts=self.radio_buttons()
 		
 		# dismostra as opções
-		self.display_options()
+		# self.display_options()
 		
 		# mostra os botoes de proximo e de quit
 		self.buttons()
 		
 		# numero de questoes
-		self.data_size=len(question)
+		self.data_size=len(self.questions)
 		
 		# contador de respostas corretas
 		self.correct=0
@@ -62,11 +65,7 @@ class Quiz:
 
 	# Esse metado checa a resposta dps de clicarmos em "próximo"
 	def check_ans(self, q_no):
-		
-		# checa se a resposta está correta
-		if self.opt_selected.get() == answer[q_no]:
-			# Se a alternativa estiver correta retorna True
-			return True
+		return self.opt_selected.get() == self.questions[self.q_no]["answer"]
 
    # Este método é usado para verificar a resposta do
    # questão atual chamando o check_ans e a pergunta no.
@@ -92,7 +91,7 @@ class Quiz:
 		else:
 			# mostra a proxima questao
 			self.display_question()
-			self.display_options()
+			# self.display_options()
 
 
 	def buttons(self):
@@ -120,7 +119,7 @@ class Quiz:
 		val=0
 		
 		# retirar marcação
-		self.opt_selected.set(1)
+		# self.opt_selected.set(1)
 		
       # repetindo as opções a serem exibidas para o
       # texto dos botões de rádio.
@@ -139,13 +138,17 @@ class Quiz:
 	# 		val+=1
    # Este método mostra a questão atual na tela
 	def display_question(self):
+		for radio_button in self.rd_buttons:
+			radio_button.destroy()
 		
-		# propriedades das questoes
-		q_no = Label(gui, text=question[self.q_no], width=60,
-			font=( 'ariel' ,16, 'bold' ), anchor= 'w')
+		current_question = self.questions[self.q_no]
 
-		
+		title = current_question["title"]
+		# propriedades das questoes
+		q_no = Label(gui, text=title, width=60,
+			font=( 'ariel' ,16, 'bold' ), anchor= 'w')
 		q_no.place(x=70, y=100)
+		self.rd_buttons = self.radio_buttons(current_question["options"])
 
 
 	# mostrar o titulo
@@ -163,8 +166,7 @@ class Quiz:
 	# na tela na posição especificada. Ele também retorna um
 	# lsit do botão de opção que será usado posteriormente para adicionar as opções a
 	# eles.
-	def radio_buttons(self):
-		
+	def radio_buttons(self, questionOptions):
       # inicializar a lista com uma lista vazia de opções
 		q_list = []
 		
@@ -172,13 +174,16 @@ class Quiz:
 		y_pos = 150
 		
 		# adicionando as opções à lista
-		while len(q_list) < 4:
+		for option in questionOptions:
 			
 			# propiedades dos botões
 			# radio_btn = Radiobutton(gui,text=" ",variable=self.opt_selected,
 			# value = len(q_list)+1, font = ("ariel",14))
-			radio_btn = Radiobutton(gui,text = '',variable=self.opt_selected,
-				value = len(q_list)+1, font = ("ariel",14), image='')
+			srcImage = Image.open(option["img"])
+			srcImage = srcImage.resize((30, 30))
+			img = ImageTk.PhotoImage(srcImage)
+			radio_btn = Radiobutton(gui,text = option["title"],variable=self.opt_selected,
+				value = len(q_list)+1, font = ("ariel",14), image=img, indicatoron=0)
 			# , imagem=PhotoImage(imagens[self.q_no])
 			
 			# adicionando botão a lista
@@ -206,15 +211,8 @@ gui.title("De onde é o som")
 with open('data.json') as f:
 	data = json.load(f)
 
-
-# definindos as perguntas e respostas
-question = (data['question'])
-options = (data['options'])
-answer = (data[ 'answer'])
-imagens = (data['imagens'])
-
 # criando obj Quiz
-quiz = Quiz()
+quiz = Quiz(data)
 
 
 gui.mainloop()
